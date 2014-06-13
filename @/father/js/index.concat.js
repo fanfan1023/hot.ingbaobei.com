dd9a5aa3f73e4a33a278a56a9808ec3aa0863124
神奇辉 <shenqihui0920@gmail.com>
2014-06-13 17:02:36 +0800

    增加音频
    增加音频、

--- @/father/js/index.concat.js
+++ @/father/js/index.concat.js
@@ -36,6 +36,107 @@ $(function() {
   // |点击进行其他页面跳转
 
 
+  /**
+   * @name reCountImgSize
+   * @desc 计算二维码位置及大小
+   * @depend ['jQuery']
+   * @return null
+   **/
+  function reCountImgSize() {
+    var bodyHeight = window.innerHeight;
+    var bodyWidth = window.innerWidth;
+    if (bodyWidth > bodyHeight) {
+      // alert('系统检测到为横屏，请使用竖屏进行查看页面。');
+    }
+
+    // qr-img
+    var qrImage = $('.page8-qr');
+    // 二维码高宽比例，当他为正方形
+    var qeIdWH = 1; // 261 / 260;
+
+    // 下面的 261 为 psd 文件二维码的宽度，1920 为画布高度， 1080为画布宽度。
+    var qrImageWidth, qrImageWidthCount, qrImageHeightCount;
+    qrImageWidthCount = 261 * bodyWidth / 1080;
+    qrImageHeightCount = 261 * bodyHeight / 1920;
+    qrImageWidth = qrImageWidthCount > qrImageHeightCount ? qrImageHeightCount : qrImageWidthCount;
+
+    qrImage.css({
+      width: qrImageWidth * qeIdWH,
+      height: qrImageWidth,
+      marginLeft: -(qrImageWidth * qeIdWH / 2),
+      marginTop: -(qrImageWidth / 2)
+    })
+    // |qr-img
+
+
+
+  }
+
+  reCountImgSize();
+  var ii = 0;
+
+  var clickEventType = ((document.ontouchstart !== null) ? 'click' : 'touchstart');
+  $('.audio-button-box').on(clickEventType, function() {
+    var self = $(this);
+    var audio = $audio.get(0);
+    // self.toggleClass("stop");
+    if (self.hasClass("stop")) {
+      audio && audio.play && audio.play();
+    } else {
+      audio && audio.stop && audio.stop();
+      audio && audio.pause && audio.pause();
+    }
+  })
+  var $audio = $('audio');
+  var audio = $audio.get(0);
+  audio && audio.play && audio.play();
+
+
+  $audio.on('ended', function() {
+    $('.audio-button-box').addClass('stop');
+  });
+  $audio.on('stop', function() {
+    $('.audio-button-box').addClass('stop');
+  });
+  $audio.on('pause', function() {
+    $('.audio-button-box').addClass('stop');
+  });
+  $audio.on('play', function() {
+    $('.audio-button-box').removeClass('stop');
+  });
+
+
+  $(document.body).one(clickEventType, function() {
+    audio && audio.play && audio.play();
+  })
+
+  if($audio.length < 1){
+    $('.audio-button-box').hide();
+  }
+
+  /**
+   * @name stopAudio
+   * @desc 停止所有音频
+   * @depend ['jQuery']
+   * @return {null}
+   **/
+  function stopAllAudio() {
+    var audio = $('audio');
+    audio.each(function(index, elem) {
+      if (elem && elem.pause) {
+        elem.pause();
+        // elem.load();
+      }
+    })
+  }
+  function closeAudio(){
+    stopAllAudio();
+    // return false;
+  }
+  window.onbeforeunload = closeAudio;
+  window.onunload = closeAudio;
+
+
 })
 
 
@@ -84,43 +185,4 @@ for (var i = imgs.length; i--;) {
       imgOlrSrc.src = imgOlrSrc.src + "?&" + (new Date()).getTime();
     }
   })();
-
-  /**
-   * @name reCountImgSize
-   * @desc 计算二维码位置及大小
-   * @depend ['jQuery']
-   * @return null
-   **/
-  function reCountImgSize() {
-    var bodyHeight = window.innerHeight;
-    var bodyWidth = window.innerWidth;
-    if (bodyWidth > bodyHeight) {
-      // alert('系统检测到为横屏，请使用竖屏进行查看页面。');
-    }
-
-    // qr-img
-    var qrImage = $('.page8-qr');
-    // 二维码高宽比例，当他为正方形
-    var qeIdWH = 1; // 261 / 260;
-
-    // 下面的 261 为 psd 文件二维码的宽度，1920 为画布高度， 1080为画布宽度。
-    var qrImageWidth, qrImageWidthCount, qrImageHeightCount; 
-    qrImageWidthCount = 261 * bodyWidth / 1080;
-    qrImageHeightCount = 261 * bodyHeight / 1920;
-    qrImageWidth = qrImageWidthCount > qrImageHeightCount? qrImageHeightCount: qrImageWidthCount;
-
-    qrImage.css({
-      width: qrImageWidth * qeIdWH,
-      height: qrImageWidth,
-      marginLeft: -(qrImageWidth * qeIdWH / 2),
-      marginTop: -(qrImageWidth / 2)
-    })
-    // |qr-img
-
-
-
-  }
-
-  reCountImgSize();
-
 }
